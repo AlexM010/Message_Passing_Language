a102658b46ab607df2f71ab69b882ef3a775c01b
Alexandros Markodimitrakis <alexandros0001@hotmail.com>
2022-01-05 14:25:41 +0200

    Functions included

--- src/MSGlang.cpp
+++ src/MSGlang.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include "MSGlang.h"
+
 Item::Item(const string& s){
         this->s = s;
         this->e=STRING;
@@ -30,6 +31,40 @@ Item::Item(Let const l){
         this->e=OBJECT;
         this->is_empty=false;
 }
+template<typename T>
+Item::Item(function<T(void)> f){
+        this->f = f;
+        this->e=METHOD;
+        this->is_empty=false;
+}
+template<typename T>
+void Item::set(function<T(void)> f){
+        this->f = f;
+}
+void Item::set(const string& s){
+        this->s = s;
+}
+void Item::set(const int& i){
+        this->i = i;
+}
+void Item::set(const double& d){
+        this->d = d;
+}
+void Item::set(const bool& b){
+        this->b = b;
+}
+void Item::set(Let l){
+        this->l = &l;
+}
+
+Item* Item::get(){
+        return this;
+}
+
+Item& Item::operator=(void* x){
+        this->set(x);
+        return *this;
+}
 
 void Item::print(){
     switch (e)
@@ -51,43 +86,60 @@ void Item::print(){
         l->print();
         break;
     case METHOD:
+        f();
         cout<<"Method";
         break;
     default:
         break;
     }
 }
-
-void Let::add(string key,const string& s){
+Let::Let(){
+    this->empty=true;
+}
+Let::Let(string s){
+    this->temp=s;
+    this->empty=false;
+}
+Let Let::add(string key,const string& s){
         Item* it=new Item(s);
         this->data.insert(pair<string,Item*>(key,it));
         this->empty=false;
+        return *this;
 }
 
-void Let::add(string key,const int& i){
+Let Let::add(string key,const int& i){
         Item* it=new Item(i);
         this->data.insert(pair<string,Item*>(key,it));
         this->empty=false;
+        return *this;
 }
 
-void Let::add(string key,const double& d){
+Let Let::add(string key,const double& d){
         Item* it=new Item(d);
         this->data.insert(pair<string,Item*>(key,it));
         this->empty=false;
+        return *this;
 }
 
-void Let::add(string key,const bool& b){
+Let Let::add(string key,const bool& b){
         Item* it=new Item(b);
         this->data.insert(pair<string,Item*>(key,it));
         this->empty=false;
+        return *this;
 }
 
-void Let::add(string key,Let const l){
+Let Let::add(string key,Let const l){
         Item* it=new Item(l);
         this->data.insert(pair<string,Item*>(key,it));
         this->empty=false;
+        return *this;
 }
-
+Let Let::add(string key,const Item& i){
+        Item* it=new Item(i);
+        this->data.insert(pair<string,Item*>(key,it));
+        this->empty=false;
+        return *this;
+}
 void Let::print(){
         cout<<"object ";
         if(this->empty){
@@ -106,7 +158,19 @@ void Let::print(){
         }
         cout << endl;
 }
+Let Let::operator[](Let x){
+        return x;
+}
+Item& Let::operator[](string key){
+        Item& item =(*this->data[key]->get());
+        return item;
+}
 
+
+Let Let::operator,(Let x){
+        this->data.insert(x.data.begin(),x.data.end());
+        return *this;
+}
 Let Let::operator,(int x){
         add(to_string(size),x);
         size++;
@@ -116,5 +180,17 @@ Let Let::operator,(int x){
 Let Let::operator+(int x){
         add(to_string(size),x);
         size++;
+        return *this;
+}
+Let Let::operator=(int x){
+        add(temp,x);
+        return *this;
+}
+Let Let::operator=(Item i){
+        add(temp,i);
+        return *this;
+}
+Let Let::operator=(function<bool(void)> f){
+        add(temp,f);
         return *this;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,21 @@
-#include <iostream>
-#include <string>
-#include <vector>
 #include "MSGlang.h"
+
+
 int main() {
-    let m = object[values 0 , 1 , 2, 3];
+    let m = object[key("s")=8,
+    key("s2")=12,
+    key("FUN")=lambda{
+        cout<<"-FUNCTION CALLED-";
+        return true;
+    },
+    func("bb"){
+        cout<<"-FUNCTION2 CALLED-"; 
+        return true;}
+    ];
+    let k=object[values 1 , 2, 3];
     m.print();
+    k.print();
+    k["2"]=input("Enter a number: ");
+    k.print();
     return 0;
 }
